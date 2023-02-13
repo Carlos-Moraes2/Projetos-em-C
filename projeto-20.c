ba71df79a49edc0f6994d2ceec0aa355bf2d3219
Carlos-Moraes2 <karlos-moraes@hotmail.com>
2023-02-12 22:43:34 -0300

    projetos 1°semestre

---
+++ projeto-20.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+float t1, t2;
+ 
+float calcularMedia(){
+    return (t1 + t2) / 2;
+    
+}
+
+int main()
+{
+   printf("\nDigite as duas temperaturas: ");
+   scanf("%f %f", &t1, &t2);
+   printf("\nA temperatura média é %.2f °C ", calcularMedia());
+
+    return 0;
+}
+
